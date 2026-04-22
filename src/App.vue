315d6e25cd72dfe7bb102c5a75fc5ca0325b3e88
zhuzhenglei <zhuzheng.lei@163.com>
2026-04-22 05:40:48 +0000

    Modify text links

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
         <a style="cursor: pointer;" class="lang-toggle" @click="toggleLang" :title="lang === 'zh' ? 'Switch to English' : '切换到中文'">
           {{ lang === 'zh' ? '简体中文' : 'EN' }}
         </a>
-        <a href="https://github.com" target="_blank" class="github-link">
+        <a href="https://github.com/zzlorz/seen" target="_blank" class="github-link">
           <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
             <path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z"/>
           </svg>
@@ -292,13 +292,10 @@
           <span class="logo-text">seen</span>
         </div>
         <p class="footer-desc">{{ t.footerDesc }}</p>
-        <div class="footer-links">
-          <a href="#">{{ t.footerGitHub }}</a>
-          <a href="#">{{ t.footerDocs }}</a>
-          <a href="#">{{ t.footerChangelog }}</a>
-          <a href="#">{{ t.footerLicense }}</a>
-        </div>
-        <p class="footer-copy">{{ t.footerCopy }}</p>
+        <p class="footer-copy">
+          {{ t.footerCopy }}
+          <a href="https://github.com/zzlorz/seen" target="_blank">{{ t.footerGitHub }}</a>
+        </p>
       </div>
     </footer>
   </div>
